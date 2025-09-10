b18e31dbc9874d711925c611de0f9a1039865076
alexdevsprint <alexdevsprint@gmail.com>
2025-09-10 19:56:34 +0300

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import css from "./Header.module.css";
 import AppNavigation from "../AppNavigation/AppNavigation";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className={css.header}>
-      <div className="container">
+      <div className={`${css.wrapper} container`}>
+        <Link to="/">
+          <img className={css.logo} src="/images/logo.png" alt="logo" />
+        </Link>
         <AppNavigation />
       </div>
     </header>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import Hero from "../../components/Hero/Hero"
 export default function HomePage() {
   return (
-    <div className={`container`}>   
+    <div >   
       <Hero />
     </div>
   );
