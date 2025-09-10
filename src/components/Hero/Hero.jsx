704c43fee6edae4b6fe40625dc791fdebf25d7e1
alexdevsprint <alexdevsprint@gmail.com>
2025-09-10 20:23:19 +0300

    fix hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import css from "./Hero.module.css";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
     <div className={css.hero}>
       <h1 className={css.title}>Find your perfect rental car</h1>
       <p className={css.desc}>Reliable and budget-friendly rentals for any journey</p>
-      <button className={css.viewCatalogBtn} type="button">View Catalog</button>
+      <Link to="/catalog" className={css.viewCatalogLink} >View Catalog</Link>
     </div>
   );
 }
