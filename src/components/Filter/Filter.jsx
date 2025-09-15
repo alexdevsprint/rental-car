a3f620a0a5d533ac0392c54639805ec83fc6fc49
alexdevsprint <alexdevsprint@gmail.com>
2025-09-15 13:30:19 +0300

    fix filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -47,32 +47,32 @@ export default function Filter() {
     label: price.toString(),
   }));
 
-const selectStyles = {
-  control: (base) => ({
-    ...base,
-    border: "none",
-    borderRadius: "12px",
-    width: "204px",
-    height: "44px",
-    backgroundColor: "#f7f7f7",
-    boxShadow: "none",
-    outline: "none",
-    cursor: "pointer",
-    paddingLeft: "16px",
-    paddingRight: "5px",
-  }),
-  menu: (base) => ({
-    ...base,
-    backgroundColor: "#fff",
-    zIndex: 10, // чтобы меню не пряталось за другими элементами
-  }),
-  option: (base, state) => ({
-    ...base,
-    backgroundColor: state.isFocused ? "#eee" : "#fff",
-    color: "#000",
-    cursor: "pointer",
-  }),
-};
+  const selectStyles = {
+    control: (base) => ({
+      ...base,
+      border: "none",
+      borderRadius: "12px",
+      width: "204px",
+      height: "44px",
+      backgroundColor: "#f7f7f7",
+      boxShadow: "none",
+      outline: "none",
+      cursor: "pointer",
+      paddingLeft: "16px",
+      paddingRight: "5px",
+    }),
+    menu: (base) => ({
+      ...base,
+      backgroundColor: "#fff",
+      zIndex: 10, // чтобы меню не пряталось за другими элементами
+    }),
+    option: (base, state) => ({
+      ...base,
+      backgroundColor: state.isFocused ? "#eee" : "#fff",
+      color: "#000",
+      cursor: "pointer",
+    }),
+  };
 
   const handleSearch = () => {
     dispatch(resetCatalog());
@@ -89,39 +89,49 @@ const selectStyles = {
 
   return (
     <div className={css.container}>
-      <Select
-        className={css.brand}
-        placeholder="Choose a brand"
-        styles={selectStyles}
-        options={brandOptions}
-        isLoading={isLoadingBrands}
-        isDisabled={isLoadingBrands}
-        onChange={(option) => dispatch(setSelectedBrand(option))}
-        value={selectedBrand}
-      />
-      <Select
-        className={css.price}
-        placeholder="Choose a price"
-        styles={selectStyles}
-        options={priceOptions}
-        onChange={(option) => dispatch(setSelectedPrice(option))}
-        value={selectedPrice}
-      />
-      <div className={css.inputContainer}>
-        <input
-          className={css.from}
-          type="number"
-          placeholder="From"
-          value={mileageFrom}
-          onChange={(e) => dispatch(setMileageFrom(e.target.value))}
+      <div className={css.fiterWrapper}>
+        <label htmlFor="brand-select">Car brand</label>
+        <Select
+          className={css.brand}
+          placeholder="Choose a brand"
+          styles={selectStyles}
+          options={brandOptions}
+          isLoading={isLoadingBrands}
+          isDisabled={isLoadingBrands}
+          onChange={(option) => dispatch(setSelectedBrand(option))}
+          value={selectedBrand}
         />
-        <input
-          className={css.to}
-          type="number"
-          placeholder="To"
-          value={mileageTo}
-          onChange={(e) => dispatch(setMileageTo(e.target.value))}
+      </div>
+
+      <div className={css.fiterWrapper}>
+        <label htmlFor="price-select">Price/ 1 hour</label>
+        <Select
+          className={css.price}
+          placeholder="Choose a price"
+          styles={selectStyles}
+          options={priceOptions}
+          onChange={(option) => dispatch(setSelectedPrice(option))}
+          value={selectedPrice}
         />
+      </div>
+      <div className={css.fiterWrapper}>
+        <label htmlFor="from-input">Сar mileage / km</label>
+        <div className={css.inputContainer}>
+          <input
+            className={css.from}
+            type="number"
+            placeholder="From"
+            value={mileageFrom}
+            onChange={(e) => dispatch(setMileageFrom(e.target.value))}
+          />
+          <input
+            className={css.to}
+            type="number"
+            placeholder="To"
+            value={mileageTo}
+            onChange={(e) => dispatch(setMileageTo(e.target.value))}
+          />
+        </div>
       </div>
 
       <button className={css.btn} type="button" onClick={handleSearch}>
